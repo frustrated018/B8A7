4f4c9d3de254747a06e398c462d722ea62f77093
frustrated018 <niloyd018@gmail.com>
2023-09-15 00:39:18 +0600

    Fixed some of the card height issue

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({ card }) => {
   return (
     <div>
       <div className="card bg-slate-300 shadow-xl">
-        <figure className="px-5 pt-10">
+        <figure className="px-4 pt-5">
           <img
             src={thumbnail}
             alt={`thumbnail for ${course_name}`}
@@ -15,11 +15,11 @@ const Card = ({ card }) => {
           />
         </figure>
         <div className="card-body items-center text-center">
-          <h2 className="card-title">{course_name}</h2>
-          <p>{course_description}</p>
+          <h2 className="card-title text-stone-900 text-lg font-semibold truncate">{course_name}</h2>
+          <p className="text-stone-900 text-opacity-60 text-sm font-normal">{course_description}</p>
           <div className=" flex justify-between gap-5">
-          <p>{price} tk</p>
-          <p>{time} hr</p>
+          <p className="text-stone-900 text-opacity-60 text-base font-medium">Price : {price} tk</p>
+          <p className="text-stone-900 text-opacity-60 text-base font-medium">Credit : {time} hr</p>
           </div>
           <div className="card-actions">
             <button className="btn btn-primary">Select</button>
