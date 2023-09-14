013d3c62768a1485f425df8556fcaa66e01bd8a0
frustrated018 <niloyd018@gmail.com>
2023-09-15 01:17:26 +0600

    Installed react icons and finished the cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,16 @@
 import PropTypes from "prop-types";
 
-const Card = ({ card }) => {
+import { FiDollarSign } from "react-icons/fi";
+import { GoBook } from "react-icons/go";
 
-    const {thumbnail, course_name, course_description, price, time} = card;
+const Card = ({ card }) => {
+  const { thumbnail, course_name, course_description, price, time } = card;
 
   return (
     <div>
       <div className=" bg-slate-300 shadow-xl rounded-xl">
         <figure className="px-4 py-5">
-            {/* Thumbnail */}
+          {/* Thumbnail */}
           <img
             src={thumbnail}
             alt={`thumbnail for ${course_name}`}
@@ -17,18 +19,34 @@ const Card = ({ card }) => {
         </figure>
         {/* Text content */}
         <div className=" flex flex-col text-center">
-            {/* Title */}
-          <h2 className="text-stone-900 text-lg font-semibold truncate mb-4">{course_name}</h2>
+          {/* Title */}
+          <h2 className="text-stone-900 text-lg font-semibold truncate mb-4">
+            {course_name}
+          </h2>
           {/* description */}
-          <p className="text-stone-900 text-opacity-60 text-sm font-normal mb-5 mx-4">{course_description}</p>
+          <p className="text-stone-900 text-opacity-60 text-sm font-normal mb-5 mx-4">
+            {course_description}
+          </p>
           {/* price & time */}
-          <div className=" flex justify-around gap-5 mb-6">
-          <p className="text-stone-900 text-opacity-60 text-base font-medium">Price : {price} tk</p>
-          <p className="text-stone-900 text-opacity-60 text-base font-medium">Credit : {time} hr</p>
+          <div className=" flex flex-row justify-evenly mb-6">
+              <div className="flex flex-row gap-1 items-center">
+                <FiDollarSign />
+                <p className="text-stone-900 text-opacity-60 text-base font-medium">
+                  Price : {price} tk
+                </p>
+              </div>
+              <div className="flex flex-row gap-1 items-center">
+                <GoBook />
+                <p className="text-stone-900 text-opacity-60 text-base font-medium">
+                  Credit : {time} hr
+                </p>
+              </div>
           </div>
           {/* Button */}
           <div className=" mb-4">
-            <button className="bg-blue-500 rounded-lg border border-blue-500  h-10 w-[90%] text-white text-lg font-semibold">Select</button>
+            <button className="bg-blue-500 rounded-lg border border-blue-500  h-10 w-[90%] text-white text-lg font-semibold">
+              Select
+            </button>
           </div>
         </div>
       </div>
