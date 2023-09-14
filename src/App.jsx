2a1d17ba59c22242fcba65852342e0a34f6f7888
frustrated018 <niloyd018@gmail.com>
2023-09-14 21:09:29 +0600

    Created fake data and added to JSON file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-stone-900 text-[32px] font-bold text-center my-12'>Course Registration</h1>
+    <Header></Header>
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <>
+      <h1 className="text-stone-900 text-[32px] font-bold text-center my-12">
+        Course Registration
+      </h1>
+    </>
+  );
+};
+
+export default Header;
