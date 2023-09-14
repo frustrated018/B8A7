d826edc60bd234807c731544f6ba20c0513389eb
frustrated018 <niloyd018@gmail.com>
2023-09-14 20:43:03 +0600

    Installed tailwind and daisy ui & imported font from google fonts

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>Initializing Project</h1>
+      <h1 className='text-stone-900 text-[32px] font-bold text-center my-12'>Course Registration</h1>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [("daisyui")],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"],
+  },
 }
 
